eee035916da354f5453b2974f0c046d4fd3bae0e
lambert34 <aleksandrlambert@gmail.com>
2023-03-28 14:18:08 +0700

    добавили вычислительные операции

--- start/index.js
+++ start/index.js
@@ -1,4 +1,5 @@
 const inputANode = document.querySelector('.js-input-a');
 const inputBNode = document.querySelector('.js-input-b');
+const selectOperationNode = document.querySelector('.js-select-operation');
 const btnResultNode = document.querySelector('.js-btn-result');
 const outputNode = document.querySelector('.js-output');
